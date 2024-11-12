e1abab813faed4a231edee5adcb02c86795a7eb1
Matthew Miller <matthew@millerti.me>
2024-11-12 15:40:30 -0800

    Scaffold out tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,20 @@
-import {} from '@std/assert';
+import { assertEquals } from '@std/assert';
 import { describe, it } from '@std/testing/bdd';
 
 import { Base64URL } from './index.ts';
 
 describe('Base64URL', () => {
   it('should encode to string', () => {
+    assertEquals(
+      Base64URL.encode(new Uint8Array([0, 0, 0, 0])),
+      'AAAAAA',
+    );
   });
 
   it('should decode to array buffer', () => {
+    assertEquals(
+      Base64URL.decode('AAAAAA'),
+      new Uint8Array([0, 0, 0, 0]).buffer,
+    );
   });
 });
